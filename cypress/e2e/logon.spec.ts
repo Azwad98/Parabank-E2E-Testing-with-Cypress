ab7e13533d7f881e78b05991b74ae208c808fb6d
Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>
2023-04-24 01:53:47 +0100

    Make use of randomly generated value for username
    The randomly generated username is imported from the temp json file generated during the registration test
    
    Signed-off-by: Mohammad Azwad Rashid <m.azwad.rashid@gmail.com>

--- cypress/e2e/logon.spec.ts
+++ cypress/e2e/logon.spec.ts
@@ -2,25 +2,22 @@ import IndexPage from '../pageObjects/index.page';
 
 describe('Login Test', () => {
   let testData: any;
-
+  let randUsername;
   before(() => {
     cy.fixture('testData').then((data) => {
       testData = data;
+    });
+    cy.readFile('cypress/fixtures/tempStorage.json').then((data) => {
+      randUsername = data.randUsername;
     });
   });
 
   it('should log in with valid credentials', () => {
     IndexPage.visit();
-    IndexPage.fillLoginForm(
-      testData.user.validUser.username,
-      testData.user.validUser.password
-    );
+    IndexPage.fillLoginForm(randUsername, testData.user.validUser.password);
     IndexPage.submitLoginForm();
 
-    cy.get('.smallText').should(
-      'contain',
-      'Welcome ' + testData.user.validUser.fullName
-    );
+    cy.get('.smallText').should('contain', 'Welcome ' + randUsername);
 
     cy.get('a[href="/parabank/logout.htm"]').click();
   });
